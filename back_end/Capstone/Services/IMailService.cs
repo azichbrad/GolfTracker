ce063c5ccdbd818a2d8f10e025d6a7e1fccb15f9
azichbrad <62947020+azichbrad@users.noreply.github.com>
2020-08-13 20:48:05 -0400

    Add files via upload

---
+++ back_end/Capstone/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
